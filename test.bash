7673c3d14639f5094ea169330050838159ddff77
uchida shou <s21c1018if@s.chibakoudai.jp>
2022-11-03 11:44:03 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,15 +1,23 @@
 #!/bin/bash
-ng(){
-	echo NG at Line $1
-	res=1
-}
-res=0
+# SPDX-FileCopyrrightText: 2022 Uchida shou
+# SPDX=License-Identifier: BSD-3-Clause
+
+
 
 out=$(seq 5 | ./plus)
-[ "${out}" = 14.0 ] || ng ${LINENO}
-[ "$res" = 0.0 ] && echo OK
-
-exit $ress
+  [ "${out}" = 15 ] || ng ${LINENO}
+      　 
+        ### STRANGE INPUT ###
+	  out=$(echo あ | ./plus)
+	    [ "$?" = 1 ]      || ng ${LINENO}
+	      [ "${out}" = "" ] || ng ${LINENO}
+	          　 
+		    out=$(echo | ./plus) #空文字
+		      [ "$?" = 1 ]      || ng ${LINENO}
+		        [ "${out}" = "" ] || ng ${LINENO}
+			    　 
+			    [ "$res" = 0 ] && echo OK
+			      exit $res
 
 
 
