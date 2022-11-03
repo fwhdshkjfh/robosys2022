7e86cee80c2c5eb9196f8083d9593ebf2f669004
uchida shou <s21c1018if@s.chibakoudai.jp>
2022-11-03 12:32:46 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,7 +1,12 @@
 #!/bin/bash
 # SPDX-FileCopyrrightText: 2022 Uchida shou
 # SPDX=License-Identifier: BSD-3-Clause
-
+ng (){
+	echo NG at Line $1
+	res=1
+}
+res=0
+### I/O TEST ###
 
 
 out=$(seq 5 | ./plus)
