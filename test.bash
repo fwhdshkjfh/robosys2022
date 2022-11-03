4d5f1cfbe81346c57537fc8b9a850ae302f1dd4e
uchida shou <s21c1018if@s.chibakoudai.jp>
2022-11-03 12:48:37 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -6,23 +6,24 @@ ng (){
 	res=1
 }
 res=0
++ res=0
+
 ### I/O TEST ###
 
 
 out=$(seq 5 | ./plus)
+++ seq 5
+++ ./plus
++ out=15
   [ "${out}" = 15 ] || ng ${LINENO}
-      　 
-        ### STRANGE INPUT ###
-	  out=$(echo あ | ./plus)
-	    [ "$?" = 1 ]      || ng ${LINENO}
-	      [ "${out}" = "" ] || ng ${LINENO}
-	          　 
-		    out=$(echo | ./plus) #空文字
-		      [ "$?" = 1 ]      || ng ${LINENO}
-		        [ "${out}" = "" ] || ng ${LINENO}
-			    　 
-			    [ "$res" = 0 ] && echo OK
-			      exit $res
+  + '[' 15 = 15 ']'
+ [ "$res" = 0 ] && echo OK
+ +'[' 0 = 0 ']'
+ + echo Ok
+ Ok
+ exit $res
+ + exit 0
+
 
 
 
