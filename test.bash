d7576e675e79668824b366dcfeefbeb3337e04b7
uchida shou <s21c1018if@s.chibakoudai.jp>
2022-11-10 12:14:48 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,29 +1,25 @@
-#!/bin/bash -xv
-# SPDX-FileCopyrrightText: 2022 Uchida shou
-# SPDX=License-Identifier: BSD-3-Clause
-
-ng (){
-	echo NG at Line $1
-	res=1
-}
-
-res=0
-+ res=0
-
+#!/bin/bash-xv
+# SPDX-FileCopyrightText: 2022 Shou Uchida
+# SPDX-License-Identifier: BSD-3-Clause
+　
+ng () {
+	      echo NG at Line $1
+	            res=1
+	    }
+	    　
+	    res=0
 out=$(seq 5 | ./plus)
-++ seq 5
-++ ./plus
-+ out=15
-[ "$?{out}" = 15 ] || ng ${LINENO}
-+ '[' 15 = 15 ']'
+[ "${out}" = 15 ] || ng ${LINENO}
 
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+  
+out=$(echo | ./plus) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+ 
 [ "$res" = 0 ] && echo OK
-+ '[' 0 = 0 ']'
-+ echo OK
-OK
 exit $res
-+ exit 0
-
-
-
 
