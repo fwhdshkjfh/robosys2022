54a79c89ff102aa91bd3a0e2b37673fab42d5d8c
uchida shou <s21c1018if@s.chibakoudai.jp>
2022-11-11 15:21:15 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -3,23 +3,22 @@
 # SPDX-License-Identifier: BSD-3-Clause
 　
 ng () {
-	      echo NG at Line $1
-	            res=1
-	    }
-	    　
-	    res=0
-out=$(seq 5 | ./plus)
+      echo NG at Line $1
+      res=1
+}
+  out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
-### STRANGE INPUT ###
+
+out=$(echo あ| ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+
 out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
+[ "$? = 1 ]       || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
-  
-out=$(echo | ./plus) #空文字
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
- 
+
 [ "$res" = 0 ] && echo OK
-exit $res
+  exit $res
 
