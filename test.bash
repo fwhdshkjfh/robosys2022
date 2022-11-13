806bc101ef8d131b900e863c5a4b10322d840baa
uchida shou <s21c1018if@s.chibakoudai.jp>
2022-11-13 20:31:01 +0900

    test

--- test.bash
+++ test.bash
@@ -1,15 +1,14 @@
 #!/bin/bash -xv
 # SPDX-FileCopyrightText: 2022 uchida shou
 # SPDX-License-Identifier: BSD-3-Clause
-　
 ng () {
 	      echo NG at Line $1
 	            res=1
 	    }
-	    　
 	    res=0
-	    out=$(echo あ | ./plus)
-   [ "${out}" = 15 ] || ng ${LINENO}	    　
+   out=$(seq 5 | ./plus)
+   [ "${out}" = 15 ] || ng ${LINENO}
+ 	    　
    out=$(echo あ | ./plus)
    [ "$?" = 1 ]      || ng ${LINENO}
    [ "${out}" = "" ] || ng ${LINENO}
