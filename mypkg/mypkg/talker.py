c1cfd0551f229fd88e6a611b0501b5da4fa91904
uchida shou <s21c1018if@s.chibakoudai.jp>
2022-11-24 10:46:36 +0900

    test

---
+++ mypkg/mypkg/talker.py
@@ -0,0 +1,18 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int16
+
+rclpy.init()
+node = Node("talker")
+pub = node.create_publisher(Int16, "countup", 10)
+n = 0
+
+def cb():
+    global n
+    msg = Int16()
+    msg.data = n
+    pub.publish(msg)
+    n += 1
+
+node.create_timer(0.5, cb)
+rclpy.spin(node)
