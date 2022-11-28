607088f90096c1c8c25eb30c5744cc5fcd00973a
uchida shou <s21c1018if@s.chibakoudai.jp>
2022-11-28 12:46:43 +0900

    test

--- test.bash
+++ test.bash
@@ -6,14 +6,14 @@ ng () {
       res=1
 }
 res=0
-   out=$(seq 5 | ./plus)
-   [ "${out}" = 15 ] || ng ${LINENO}
-   out=$(echo あ | ./plus)
-   [ "$?" = 1 ]      || ng ${LINENO}
-   [ "${out}" = "" ] || ng ${LINENO}
-   out=$(echo | ./plus)
-   [ "$?" = 1 ]      || ng ${LINENO}
-   [ "${out}" = "" ] || ng ${LINENO}
+  out=$(seq 5 | ./plus)
+  [ "${out}" = 15 ] || ng ${LINENO}
+  out=$(echo あ | ./plus)
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+  out=$(echo | ./plus)
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
 [ "$res" = 0 ] && echo OK
   exit $res
 
