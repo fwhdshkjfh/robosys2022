26a9e0cc23788c50dcc8ed6bcce0233c168a9854
uchida shou <s21c1018if@s.chibakoudai.jp>
2022-12-01 15:42:30 +0900

    test

--- test.bash
+++ test.bash
@@ -5,14 +5,20 @@
         echo NG at  Line  $1
         res=1
   }
+
   res=0
+
+
   out=$(seq 5 | ./plus)
   [ "${out}" = 15 ] || ng ${LINENO}
+
   out=$(echo あ | ./plus)
   [ "$?" = 1 ]      || ng ${LINENO}
   [ "${out}" = "" ] || ng ${LINENO}
+
   out=$(echo | ./plus) 
   [ "$?" = 1 ]      || ng ${LINENO}
   [ "${out}" = "" ] || ng ${LINENO}
+
 [ "$res" = 0 ] && echo OK
   exit $res
